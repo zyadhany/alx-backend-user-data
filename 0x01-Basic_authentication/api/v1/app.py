1bd7fe0ce67f4cfc247ca2477e45e0ac17886f95
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-09-30 10:21:06 +0000

    AUTH

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,6 +13,10 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+auth = None
+if os.getenv('AUTH_TYPE') == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -33,6 +37,22 @@ def unauthorized(error) -> str:
     """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
+
+
+forbid_list = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+
+@app.before_request
+def before_request() -> str:
+    """ Before request
+    """
+    if auth is None:
+        return
+    if not auth.require_auth(request.path, forbid_list):
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -20,7 +20,9 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """Authorization header"""
-        return None
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Current user"""
