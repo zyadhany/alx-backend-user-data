c36b9a41c842975b909df232d033cf66344ccce5
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-09-30 10:54:04 +0000

    AUTH

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Auth class to manage the API authentication"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """BasicAuth class to manage the API authentication"""
+
+    pass
