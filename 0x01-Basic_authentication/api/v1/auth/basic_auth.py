59ee1110bade36a29d3798fbf588eb02266df75a
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-09-30 10:57:45 +0000

    auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,13 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """BasicAuth class to manage the API authentication"""
 
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Extract base64 authorization header"""
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[6:]
