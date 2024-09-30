adf033fa2e8052f47f05e2d106430112bf01e34f
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-09-30 10:10:16 +0000

    more pointer

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,10 +9,13 @@ class Auth:
     """Auth class to manage the API authentication"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Require authentication"""
         return False
 
     def authorization_header(self, request=None) -> str:
+        """Authorization header"""
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
+        """Current user"""
         return None
