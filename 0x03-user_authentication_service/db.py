09a2f11a08ff163d2456a1c42c656c1c41dc61c0
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-10-01 22:27:27 +0000

    user auth

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -41,4 +43,14 @@ class DB:
     def find_user_by(self, **kwargs) -> User:
         """Find a user by a given keyword argument
         """
-        return self._session.query(User).filter_by(**kwargs).first()
+
+        for key in kwargs.keys():
+            if not hasattr(User, key):
+                raise InvalidRequestError
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+
+        if user is None:
+            raise NoResultFound
+
+        return user
