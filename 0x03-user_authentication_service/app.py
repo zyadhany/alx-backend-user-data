4e7d2a0f9265ee42dc09670dc48071856a3c668b
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-10-01 23:53:47 +0000

    user auth

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -71,6 +71,21 @@ def get_reset_password_token() -> str:
         abort(403)
 
 
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
+def update_password() -> str:
+    """
+    PUT /reset_password
+    """
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"})
+    except ValueError:
+        abort(403)
+
+
 @app.route("/profile", methods=["GET"], strict_slashes=False)
 def profile() -> str:
     """
