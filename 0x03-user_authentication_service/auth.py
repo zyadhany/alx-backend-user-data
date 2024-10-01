ea6c581410b6c3a2cb505c6dc78acca698a459f8
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-10-01 22:35:55 +0000

    user auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,8 +3,29 @@
 Auth module
 """
 
+from db import DB
+from user import User
+
 
 def _hash_password(password: str) -> str:
     """Returns a hashed password"""
     import bcrypt
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a new user
+        """
+
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f'User {email} already exists')
+        except Exception:
+            return self._db.add_user(email, _hash_password(password))
