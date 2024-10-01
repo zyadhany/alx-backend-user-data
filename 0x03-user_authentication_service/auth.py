d9da2e54925c5f3d3b2e51f020287247f619366b
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-10-01 23:42:17 +0000

    user auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -56,10 +56,13 @@ class Auth:
     def get_user_from_session_id(self, session_id: str) -> User:
         """Get a user from a session ID
         """
+        if session_id is None:
+            return None
         try:
-            return self._db.find_user_by(session_id=session_id)
+            user = self._db.find_user_by(session_id=session_id)
         except Exception:
             return None
+        return user
 
     def destroy_session(self, user_id: int) -> None:
         """Destroy a session
