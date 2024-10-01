79211e160fa0bdffe348c82b4aeedb7b21c30842
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-10-01 19:58:27 +0000

    seassion auth

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -40,3 +40,15 @@ def auth_session_login() -> str:
     response = jsonify(user.to_json())
     response.set_cookie(SEASSION_NAME, session_id)
     return response
+
+
+@app_views.route('/auth_session/logout/', methods=['DELETE'],
+                 strict_slashes=False)
+def auth_session_logout() -> str:
+    from api.v1.app import auth
+
+    statue = auth.destroy_session(request)
+
+    if not statue:
+        abort(404)
+    return jsonify({}), 200
