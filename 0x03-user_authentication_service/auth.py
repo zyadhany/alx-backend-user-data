063f3f4fe6550376d0c7e1f80831eae513a40a19
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-10-01 23:44:18 +0000

    user auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -68,3 +68,11 @@ class Auth:
         """Destroy a session
         """
         self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Get a reset password token
+        """
+        user = self._db.find_user_by(email=email)
+        reset_token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
