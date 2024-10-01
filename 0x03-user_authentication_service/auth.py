121156bbd81e45389a8daccf5ecc8c66beb1e3a2
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-10-01 22:55:49 +0000

    user auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -28,3 +28,12 @@ class Auth:
         except Exception:
             return self._db.add_user(email, _hash_password(password))
         raise ValueError("User {} already exists".format(email))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate user credentials
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return user.hashed_password == _hash_password(password)
+        except Exception:
+            return False
