8b7a201db8b81be07b155aa5ee1e11f1aaf5d554
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-10-01 23:07:57 +0000

    user auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -52,3 +52,11 @@ class Auth:
         session_id = _generate_uuid()
         self._db.update_user(user.id, session_id=session_id)
         return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """Get a user from a session ID
+        """
+        try:
+            return self._db.find_user_by(session_id=session_id)
+        except Exception:
+            return None
