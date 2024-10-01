afc3fdfb46f8269cdebe2493c5a5a4a5b360c0ff
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-10-01 19:05:13 +0000

    seassion auth

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Auth class to manage the API authentication"""
+
+from api.v1.auth.auth import Auth
+from typing import TypeVar
+
+
+class SessionAuth(Auth):
+    """Session Auth class"""
+    pass
