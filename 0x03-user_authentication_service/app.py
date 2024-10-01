90c2098bbce7f35a16a072b3ea8fc07f1109ddde
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-10-01 23:15:03 +0000

    user auth

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -48,6 +48,7 @@ def logout() -> str:
     """
     DELETE /sessions
     """
+    session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
     if not user:
         abort(403)
