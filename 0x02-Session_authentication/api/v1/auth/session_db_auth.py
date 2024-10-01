7db01392ffa48385cdf79e2db250349b39b6afc3
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-10-01 20:32:41 +0000

    seassion auth

---
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""Auth class to manage the API authentication"""
+
+from api.v1.auth.session_auth import SessionAuth
+from typing import TypeVar
+from uuid import uuid4
+from os import getenv
+from models.user import User
+from models.user_session import UserSession
+import datetime
+
+SESSION_DURATION = getenv('SESSION_DURATION')
+
+
+class SessionDBAuth (SessionAuth):
+    """ Session DB Auth class """
+    def create_session(self, user_id=None):
+        """ Creates a Session ID for a user_id """
+        session_id = super().create_session(user_id)
+        if session_id is None:
+            return None
+        user_session = UserSession(user_id=user_id, session_id=session_id)
+        user_session.save()
+        return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """ Returns a User ID based on a Session ID """
+        if session_id is None:
+            return None
+        user_sessions = UserSession.search({'session_id': session_id})
+        if not user_sessions:
+            return None
+        user_session = user_sessions[0]
+        if self.session_duration <= 0:
+            return user_session.user_id
+        if user_session.created_at + \
+           datetime.timedelta(seconds=self.session_duration) \
+           < datetime.datetime.now():
+            return None
+        return user_session.user_id
+
+    def destroy_session(self, request=None):
+        """ Deletes the user session / logout """
+        if request is None:
+            return False
+        session_id = self.session_cookie(request)
+        if not session_id:
+            return False
+        user_sessions = UserSession.search({'session_id': session_id})
+        if not user_sessions:
+            return False
+        user_sessions[0].remove()
+        return True
